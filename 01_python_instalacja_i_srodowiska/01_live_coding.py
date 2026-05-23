82d74b97378bf1638f9acaeff1197f596d999cd8
Kamil Bartocha <kamilbartocha53@gmail.com>
2026-05-23 10:47:55 +0200

    01 after classes

---
+++ 01_python_instalacja_i_srodowiska/01_live_coding.py
@@ -0,0 +1,27 @@
+# pierwszy program - klasyczny Hello World
+print("Hello, World!")
+
+x = "Ala ma kota"
+
+print(x)
+
+
+
+###################
+
+x = "Alice"
+print(x)
+
+x = "Bob"
+print(x)
+
+x = 4
+
+print(x)
+
+print("Ala ma kota")
+
+
+print("Hello World!")
+name = "Alice"
+print("Cześć", name)
