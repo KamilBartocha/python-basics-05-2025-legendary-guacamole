6d62d6d551ab4e8cef129d7599bd8322f66141b0
Kamil Bartocha <kamilbartocha53@gmail.com>
2026-05-23 15:43:16 +0200

    add 03 after classes

---
+++ 03_python_kolekcje_i_sterowanie/03_live_coding.py
@@ -0,0 +1,34 @@
+x = [0, 1 ,"ala"]
+print(x)
+
+
+# list_a = [9, 8, 1, 2, 5, 7]
+
+# list_a_sort = sorted(list_a)
+# print(list_a)
+# print(list_a_sort)
+
+
+
+list_a = [9, 8, 1, 2, 5, 7]
+list_a.sort()
+print(list_a)
+
+
+list_a.append(0)
+print(list_a)
+
+list1 = [1, 1, 1]
+list2 = [2, 2, 2]
+list3 = [3, 3, 3]
+
+list_d = [list1, list2, list3]
+print(list_d)
+
+"""
+ [
+    [1, 1, 1],
+    [2, 2, 2],
+    [3, 3, 3]
+ ]
+"""
